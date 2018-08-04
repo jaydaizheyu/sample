22b3c1a7ebac177f58e78a91587d84590db7e6cd
戴哲瑜 <jaydaizheyu@163.com>
2018-08-04 14:38:13 +0800

    added print

--- plotR.R
+++ plotR.R
@@ -1,3 +1,4 @@
 library(ggplot2)
 data(mpg)
-ggplot(data=mpg,aes(x=displ,y=hwy,color=manufacturer))+geom_point() 
+ggplot(data=mpg,aes(x=displ,y=hwy,color=manufacturer))+geom_point()
+print("okey~~")
